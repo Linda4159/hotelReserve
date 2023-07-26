13b08a5407ddccf9ff4e0a5ccbf502ea526bc0dd
debyrise <deborahoby693@gmail.com>
2023-07-26 04:58:28 +0100

    regiration form added

---
+++ Sign Up/script.js
@@ -0,0 +1,23 @@
+document.getElementById("ntn-reg").addEventListener("click", function(event){
+    event.preventDefault()
+    let hot = document.getElementById("hotelname").value
+    let hoon = document.getElementById("useremail").value
+    let hoot = document.getElementById("PhoneNo").value
+    let htto = document.getElementById("state").value
+    let hott = document.getElementById("userpassword").value
+    let hoto = document.getElementById("Address").value
+
+    if(hot.trim().length ===0 || hoon.trim().length ===0 || hoot.trim().length ===0 || htto.trim().length===0 || hott.trim().length===0 || hoto.trim().length===0)
+     {
+        alert("please fill all field ")
+     }else
+     {
+        localStorage.setItem("hotelname",hot)
+        localStorage.setItem("useremail",hoon)
+        localStorage.setItem("phone no",hoot)
+        localStorage.setItem("state",htto)
+        localStorage.setItem("userpassword",hott)
+        localStorage.setItem("adress",hoto)
+
+     }
+})
